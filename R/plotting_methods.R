b06cfe2f9c6841b891279dd00b0beec114c0d52a
Alain Danet <alain.danet@mnhn.fr>
2023-11-23 17:30:52 +0000

    Add a method figure

--- R/plotting_methods.R
+++ R/plotting_methods.R
@@ -12,3 +12,132 @@ rho_cat_colors <- function() {
 resp_div_cat_colors <- function() {
   rho_cat_colors()
 }
+
+timespecies_mat_to_long_tibble <- function(x) {
+  x %>%
+    as_tibble() %>%
+    mutate(timestep = seq_len(nrow(.))) %>%
+    pivot_longer(
+      -timestep,
+      names_to = "species",
+      values_to = "biomass"
+    )
+}
+
+plot_timeseries_lg_tbl <- function(x, text_font_size = 8) {
+  x %>%
+    ggplot(aes(x = timestep, y = biomass, color = species)) +
+    geom_line() +
+    labs(x = "Timesteps", y = "Biomass") +
+    theme_half_open(font_size = text_font_size) +
+    theme(legend.position = "none")
+
+}
+
+get_stab_fw_label <- function(
+  x = sim_for_plot,
+  id = 1,
+  var = c(
+    "richness", "Z", "ct_alive", "w_avg_tlvl", "avg_int_strength",
+    "stab_com", "pop_stab", "async", "sae_total", "cpe"
+    ),
+  sep = ": "
+  ) {
+  out <- x %>%
+    filter(sim_id == id) %>%
+    select(where(is.numeric)) %>%
+    pivot_longer(everything()) %>%
+    deframe()
+  out <- out[var]
+  paste0(
+    var_replacement()[names(out)], sep,
+    map_chr(signif(out, digits = 2), ~format(.x, scientific = FALSE)),
+    collapse = "\n"
+)
+}
+
+get_mat_list <- function(x, var = species) {
+  out <- x %>%
+    pull({{var}})
+  names(out) <- x %>%
+    pull(sim_id)
+  out
+}
+
+# Code to plot network
+source("https://raw.githubusercontent.com/alaindanet/fishcom/master/R/codeWeb.R")
+
+plot_sim <- function(x = sim_for_plot,
+  id = 6827,
+  col_palette = NULL,
+  node_size_scale = .5,
+  font_size = 10
+  ) {
+  # get the good sim
+  sim <- x %>%
+    filter(sim_id == id)
+  # Get the matrix
+  mat <- get_mat_list(sim)[[1]]
+
+  # Get species color
+  if (is.null(col_palette)) {
+    col_palette <- paletteer::paletteer_dynamic("cartography::multi.pal", ncol(mat))
+    col_palette <- setNames(col_palette, paste0("V", seq(1, ncol(mat))))
+  }
+
+  # Make the timeseseries plot
+  ts_plot <- timespecies_mat_to_long_tibble(mat) %>%
+    plot_timeseries_lg_tbl(., text_font_size = font_size) +
+    scale_color_manual(values = col_palette) +
+    scale_y_continuous(trans = "log10")
+
+  # Add the stability and food-web metrics
+  ts_plot_metric <- ts_plot +
+    coord_cartesian(
+      expand = FALSE,
+      # Do not cut the annotations below
+      clip = "off") +
+    # This widens the right margin
+    theme(plot.margin = unit(c(1, 12, 1, 1), "lines")) +
+    annotate("text", x = 202, y = 0,
+      label = get_stab_fw_label(x = x, id = id),
+      #https://stackoverflow.com/a/65077171
+      size = font_size / .pt - 1,
+      hjust = 0,
+      lineheight = .8,
+      vjust = 0
+    )
+
+  # Get the Food-web
+  ## Adjency matrix
+  int_mat <- get_mat_list(sim, var = int_strength)[[1]]
+  adj_mat <- int_mat > 0
+  TL <- NetIndices::TrophInd(t(adj_mat))$TL
+  ## Plot the food-web
+  p <- function(abun = colMeans(mat)) {
+    par(
+      mar = c(0, 0, 0, 0),
+      mgp = c(0, 0, 0),
+      pty = "s"
+    )
+    PlotWeb(
+      TL = TL,
+      webTL = t(adj_mat),
+      colnode = col_palette,
+      abund = abun,
+      collink = "grey70",
+      scale_abun = node_size_scale
+    )
+  }
+
+  # Combine timeseries and food-web
+  ggdraw(ts_plot_metric) +
+    draw_plot(p,
+      x = 1, y = 1,
+      scale = .4,
+      hjust = 1, vjust = 1,
+      halign = 1,
+      valign = 1
+    )
+
+}
