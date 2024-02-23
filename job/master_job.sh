cbfbdc3bb03a1b3d2a45c5deaf9e28b78812ab2a
Alain Danet <alain.danet@mnhn.fr>
2024-02-23 12:17:13 +0000

    Add master bash to rerun sim

---
+++ job/master_job.sh
@@ -0,0 +1,4 @@
+sbatch csh_allo_d_stanage_array_no_rerun.sh
+sbatch csh_allo_d_stanage_array_no_rerun_no_disconnected.sh
+sbatch csh_allo_d_stanage_array_no_rerun_short.sh
+sbatch csh_non_allo_d_stanage_array_no_rerun.sh
